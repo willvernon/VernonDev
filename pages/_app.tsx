1f6005624f379bc985a4a30160e82f15479666de
Vernon <wmvernon@proton.me>
2022-11-12 16:51:33 -0500

    font-mono

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,21 @@ import 'styles/global.css';
 import { useState } from 'react';
 import { ThemeProvider } from 'next-themes';
 import { SessionProvider } from 'next-auth/react';
-import { Inter } from '@next/font/google';
+
 import { Analytics } from '@vercel/analytics/react';
 
-const interVariable = Inter();
+
 
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps }
+  
 }) {
   return (
     <SessionProvider session={session}>
       <ThemeProvider attribute="class">
-        <main className={interVariable.className}>
+        <main className="font-mono">
           <Component {...pageProps} />
           <Analytics />
         </main>
