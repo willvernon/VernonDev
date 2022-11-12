5703058d972bf8d87a2c43bfa5019b9b48a4009e
Vernon <wmvernon@proton.me>
2022-11-12 15:57:48 -0500

    404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,17 +6,16 @@ import Container from 'components/Container';
 export default function NotFound() {
   return (
     <Container title="404 – Will Vernon">
-      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+      <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 text-center">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-          451 – Unavailable For Legal Reasons
+          404 – You Seem Lost
         </h1>
         <p className="text-gray-600 dark:text-gray-400 mb-8">
-          Why show a generic 404 when I can make it sound mysterious? It seems
-          you&apos;ve found something that used to exist, or you spelled
-          something wrong. I&apos;m guessing you spelled something wrong. Can
-          you double check that URL?
+          It seems you&apos;ve found something that used to exist, or
+          you are trying to time travel to a page that doesn&apos;t quite
+          exist yet.
         </p>
-        <div className='m-auto mb-10'>
+        <div className="m-auto mb-10">
           <Image
             src={Travolta}
             alt="Travolta"
