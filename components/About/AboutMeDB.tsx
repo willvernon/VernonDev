61e25cc3ce39cbb1e6c5e63d56f12472347036a0
Vernon <wmvernon@proton.me>
2022-11-13 14:01:28 -0500

    add-about grid

--- components/About/AboutMeDB.tsx
+++ components/About/AboutMeDB.tsx
@@ -6,10 +6,20 @@ import Link from 'next/link';
 
 function AboutMeDB() {
   return (
-    <div className="grid grid-cols-8 grid-rows-3 w-full h-full mt-12 bg-[#191919] rounded-2xl">
+    <div className="grid grid-cols-8 grid-rows-3 w-full h-full my-12 bg-[#191919] rounded-2xl">
+      {/* About Me Box */}
       <div className="row-start-1 row-span-2 col-start-1 col-span-4 m-1 rounded-xl bg-white text-center">
-        <h1 className="text-black">About Me</h1>
+        <div className="w-auto h-44 bg-white mt-32  border-b border-slate-500 text-center">
+          {' '}
+          <p className="text-slate-500 font-mono text-center pt-4 font-black text-3xl tracking-widest">
+            About Me
+          </p>
+        </div>
+        <div className=" text-black font-bold  ">
+          <p className='p-3 m-auto'>Coder-Designer-Tech-Sports</p>
+        </div>
       </div>
+      {/* Computer Image Box */}
       <div className="row-start-1 row-span-1 col-start-5 col-span-2 m-1 rounded-xl bg-transparent h-25 w-25 text-center">
         <Image
           src={Computer}
@@ -18,6 +28,7 @@ function AboutMeDB() {
           className="rounded-xl opacity-80"
         />
       </div>
+      {/* Mobile Mini Nav */}
       <div className="row-start-1 row-span-1 col-start-7 col-span-2 m-1 rounded-xl bg-transparent h-25 w-25 text-center">
         <ul className="flex flex-col absolute py-10">
           <li
@@ -54,17 +65,52 @@ function AboutMeDB() {
           </li>
         </ul>
       </div>
+      {/* Tilted Blocks My Projects */}
       <div className="row-start-2 row-span-1 col-start-5 col-span-2 m-1 rounded-xl bg-transparent h-25 w-25 text-center">
-        <div className="w-auto h-14 bg-orange-400 rounded-xl text-left pl-20 pt-3 font-mono text-xs text-slate-700 font-black"><p>My <br /> Projects</p></div>
-        <div className="w-auto h-14 bg-white rounded-xl rotate-12 -translate-x-2 shadow-sm shadow-black text-center"> <p className='text-slate-500 font-mono text-center pt-4 font-black text-lg tracking-widest'>VernonDev</p></div>
-        <div></div>
+        <div className="w-auto h-14 bg-orange-400 rounded-xl text-left pl-3 pt-3 font-mono text-xs text-slate-700 font-black">
+          <Link
+            href="/blog"
+            className="flex items-center mt-1 text-slate-800  rounded-lg hover:text-gray-800  transition-all h-6"
+          >
+            <>
+              {'My'}
+              <br />
+              {'Projects'}
+              <div className="ml-7 mt-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="h-6 w-6 ml-1"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
+                  />
+                </svg>
+              </div>
+            </>
+          </Link>
+        </div>
+        <div className="w-auto h-14 bg-white rounded-xl rotate-12 -translate-x-2 shadow-sm shadow-black text-center">
+          {' '}
+          <p className="text-slate-500 font-mono text-center pt-4 font-black text-lg tracking-widest">
+            VernonDev
+          </p>
+        </div>
       </div>
+      {/* Box 5 */}
       <div className="row-start-2 row-span-1 col-start-7 col-span-2 m-1 rounded-xl bg-white h-25 w-25 text-center">
         Box 5
       </div>
+      {/* Box 6 */}
       <div className="row-start-3 row-span-1 col-start-1 col-span-2 m-1 rounded-xl bg-transparent h-25 w-25 text-center">
         Box 6
       </div>
+      {/* MeMoji Green */}
       <div className="row-start-3 row-span-1 col-start-3 col-span-2 m-1 rounded-xl bg-gradient-to-r from-emerald-600 to-emerald-800 h-25 w-25 text-center">
         <Image
           src={MeMoji}
@@ -74,8 +120,32 @@ function AboutMeDB() {
           className="m-auto h-full object-center py-5"
         />
       </div>
-      <div className="row-start-3 row-span-1 col-start-5 col-span-4 m-1 rounded-xl bg-white h-25 w-25 text-center">
-        Box 8
+      {/* Box 8 */}
+      <div className="row-start-3 row-span-1 col-start-5 col-span-4 m-1 rounded-xl bg-white h-25 w-25 text-left">
+        <div className="flex text-black m-3 text-xs">
+          <ul className="p-2">
+            <h2 className="text-lg pb-2">Links</h2>
+            {/* <li>
+                Twitter: <a href="https://twitter.com/">@---</a>
+              </li> */}
+            <li className="py-1">
+              GitHub: <a href="https://github.com/willvernon">@willvernon</a>
+            </li>
+            <li className="py-1">
+              Website: <Link href="https://vernondev.com">vernondev.com</Link>
+            </li>
+            <li className="py-1">
+              LinkedIn:{' '}
+              <a href="https://www.linkedin.com/in/wmvernon/">/wmvernon</a>
+            </li>
+            <li className="py-1">
+              ENS:{' '}
+              <a href="https://app.ens.domains/name/vernondev.eth/details">
+                Vernnondev.eth
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- pages/about.tsx
+++ pages/about.tsx
@@ -13,9 +13,7 @@ export default function About() {
   return (
     <Container title="About – Will Vernon">
       <div className="flex flex-col justify-center items-start max-w-4xl mx-auto mb-16 w-full">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-          About Me
-        </h1>
+        
         <AboutMeDB />
         <div className="mb-8 prose dark:prose-dark leading-6">
           <div className="flex">
