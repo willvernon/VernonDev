dc15c9f47f95693aab0a8837e46cbaf6feaf3e68
Vernon <wmvernon@proton.me>
2022-11-23 14:35:13 -0500

    added blog page link to featured posts

--- components/BlogPostCard.tsx
+++ components/BlogPostCard.tsx
@@ -3,17 +3,26 @@ import useSWR from 'swr';
 import cn from 'classnames';
 
 export default function BlogPostCard({ title, slug, gradient }) {
- 
-
-  return (
-    <Link
-      href={`/blog/${slug}`}
+  const ExternalLink = ({ href, children }) => (
+    <a
       className={cn(
         'transform hover:scale-[1.01] transition-all',
         'rounded-xl w-full md:w-1/3 bg-gradient-to-r p-1',
         gradient
       )}
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
     >
+      {children}
+    </a>
+  );
+  {
+    `https://innominate-coder.vercel.app/${slug}`;
+  }
+
+  return (
+    <ExternalLink href="https://innominate-coder.vercel.app">
       <div className="flex flex-col justify-between h-full bg-white dark:bg-gray-900 rounded-lg p-4">
         <div className="flex flex-col md:flex-row justify-between">
           <h4 className="text-lg md:text-lg font-medium mb-6 sm:mb-10 w-full text-gray-900 dark:text-gray-100 tracking-tight">
@@ -41,11 +50,9 @@ export default function BlogPostCard({ title, slug, gradient }) {
               d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
             />
           </svg>
-          <span className="ml-2 align-baseline capsize">
-            
-          </span>
+          <span className="ml-2 align-baseline capsize"></span>
         </div>
       </div>
-    </Link>
+    </ExternalLink>
   );
 }
